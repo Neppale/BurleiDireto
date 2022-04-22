43a5226221447751e8e66a293a9760ae3ad20044
Neppale <gabrielverissimodias@gmail.com>
2022-04-22 13:13:38 -0300

    feat: quality of life changes
    - Changed button to toggle
    - Changed popup
    - Added icons

--- js/burleidireto.js
+++ js/burleidireto.js
@@ -1,32 +1,47 @@
-console.log("[BurleiDireto] I'm activated.");
+console.log("[BurleiDireto] Active!");
+removeBlur();
 
 document.addEventListener("DOMContentLoaded", function () {
+  chrome.storage.local.get(['switchOption'], function(data) { // Pegar switchOption
+    console.log('[BurleiDireto] Settings retrieved: ' + data.switchOption); // Debug message
+    data.switchOption ? document.getElementById("activate").checked = true : document.getElementById("activate").checked = false // Mudar switch para o switchOption
+  });
+
   document.getElementById("activate").addEventListener("click", async () => {
+    var switchOption = document.getElementById("activate").checked;
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true, url: "https://www.passeidireto.com/*" });
+
+    chrome.storage.local.set({'switchOption': switchOption}, function() {
+      console.log('[BurleiDireto] Settings saved: ' + switchOption); // Debug message
+    });
+
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: removeBlur,
     });
   });
-})
+});
 
-
+// removeBlur
 
 function removeBlur() {
-    try {
-      console.log("[BurleiDireto] Button on.");
-      const head = document.getElementsByTagName('HEAD')[0];
-      const link = document.createElement('link');
-      link.rel = 'stylesheet'; 
-      link.type = 'text/css';
-      link.href = 'https://www.verissimo.dev/api/styles.css'; 
-      head.appendChild(link);
-      return true; 
+  console.log('[BurleiDireto] Function ran.'); // Debug message
+  chrome.storage.local.get(['switchOption'], function(data) {
+    if (data.switchOption) {
+        console.log("[BurleiDireto] Switch on.");
+        const head = document.getElementsByTagName('HEAD')[0];
+        const link = document.createElement('link');
+        link.rel = 'stylesheet'; 
+        link.type = 'text/css';
+        link.href = 'https://www.verissimo.dev/api/styles.css'; 
+        head.appendChild(link);
   
-    } catch (error) {
-      console.log("[BurleiDireto] Button on, but something wrong happened.");
-      return false;
+    } else {
+      // Should remove injected css. SOON!
     }
+  });
+
+
 
 }
 
